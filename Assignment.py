4320f0d641682e2b797a62dc5a156a6c46fee3f9
Peblusso <Priscillaebenedict@gmail.com>
2022-02-01 05:49:11 -0800

    Adding my file

---
+++ Assignment.py
@@ -0,0 +1,27 @@
+#Task
+#Use the Get method to print the value of the model key of the car dictionary.
+car = {
+   "brand": "Ford",
+    "model" : "Mustang",
+    "year" : 1964,
+}
+
+maker = car.get("model")
+print(f"The model of my car is {maker}.")
+
+
+#Qestion 2 
+# A given object may apper in a list mopre than once
+
+#Question 3
+User_info =  {
+    "Name": "Priscilla Benedict",
+    "Username": "@Mum_janie",
+    "Bio": "Machala Stan",
+    "Location": "Somewhere in West Africa",
+    "D.O.B": "July 29",
+    "Instagram": "Peb_lusso",
+    "Following": 1096,
+    'Followers': 1560
+}
+print(User_info)
